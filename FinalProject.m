d3313f97ae44a874ab2683f0fa9a13f2f8657538
ajg439 <ajg439@drexel.edu>
2024-03-14 21:39:56 -0400

    Added stitching and blending for the two images

--- FinalProject.m
+++ FinalProject.m
@@ -3,16 +3,15 @@
 clear variables;
 close all;
 
-im = double(imread("first.jpg"))/255;
-im2 = double(imread("second.jpg"))/255;
+im = imread("first.jpg");
+im2 = imread("second.jpg");
 
 % PREPROCESSING - Rotate images to vertical orientation
 im = imrotate(im, -90);
 im2 = imrotate(im2, -90);
 
-[im, im2, points, points2] = point_correspondences(im, im2);
-im = uint8(im*255);
-im2 = uint8(im2*255);
+[imMarked, im2Marked, points, points2] = point_correspondences(im, im2);
+
 figure(1);
 imshow(im);
 figure(2);
@@ -22,18 +21,20 @@ imshow(im2);
 % QUESTION 1 %
 %%%%%%%%%%%%%%
 figure('Name','Point Correspondence Side-by-Side', 'FileName','PointCorrespondence.jpg');
-imshowpair(im,im2,'montage');
+imshowpair(imMarked,im2Marked,'montage');
 
 %%%%%%%%%%%%%%
 % QUESTION 2 %
 %%%%%%%%%%%%%%
 transformationMatrix = FindTransformationMatrixWithPoints(points, points2);
-
+stitchedImage = StitchImages(im, im2, transformationMatrix);
+figure('Name','Stitched Image', 'FileName','StitchedImage.jpg');
+imshow(stitchedImage);
 
 % 1 (10 points) Hard Coding Point Correspondences
 function [im, im2, points, points2] = point_correspondences(im, im2)
-    points = [1390 135; 2177 875; 1205 2045; 2643 2983];
-    points2 = [665 86; 1535 947; 473 2032; 1839 2995];
+    points = [2188 943; 1193 2036; 1125 3592; 2660 3585];
+    points2 = [1541 1024; 463 2026; 257 3668; 1774 3577];
     radius = 20;
     colors = {'red', 'yellow', 'blue', 'green'};
     for i = 1:size(points,1)
@@ -43,7 +44,7 @@ function [im, im2, points, points2] = point_correspondences(im, im2)
 end
 
 % Get the transformation matrix using points
-function homogenousMatrix = FindTransformationMatrixWithPoints(points, points2)
+function homogenousMatrix = FindTransformationMatrixWithPoints(basePoints, primePoints)
     %Note: Points are still in (x,y) notation
     
     homogenousMatrix = zeros(3,3);
@@ -51,16 +52,16 @@ function homogenousMatrix = FindTransformationMatrixWithPoints(points, points2)
     B = [];
     rowNum = 1;
     for pointNum = 1:4
-        A(rowNum, :) = [points(pointNum, 1) points(pointNum, 2) 1 0 0 0 0 0 0];
-        B(rowNum, :) = [0 0 0 0 0 0 (points(pointNum, 1) * points2(pointNum, 1)) (points(pointNum, 2) * points2(pointNum, 1)) points2(pointNum, 1)];
+        A(rowNum, :) = [basePoints(pointNum, 1) basePoints(pointNum, 2) 1 0 0 0 0 0 0];
+        B(rowNum, :) = [0 0 0 0 0 0 (basePoints(pointNum, 1) * primePoints(pointNum, 1)) (basePoints(pointNum, 2) * primePoints(pointNum, 1)) primePoints(pointNum, 1)];
         rowNum = rowNum + 1;
-        A(rowNum, :) = [0 0 0 points(pointNum, 1) points(pointNum, 2) 1 0 0 0];
-        B(rowNum, :) = [0 0 0 0 0 0 (points(pointNum, 1) * points2(pointNum, 2)) (points(pointNum, 2) * points2(pointNum, 2)) points2(pointNum, 2)];
+        A(rowNum, :) = [0 0 0 basePoints(pointNum, 1) basePoints(pointNum, 2) 1 0 0 0];
+        B(rowNum, :) = [0 0 0 0 0 0 (basePoints(pointNum, 1) * primePoints(pointNum, 2)) (basePoints(pointNum, 2) * primePoints(pointNum, 2)) primePoints(pointNum, 2)];
         rowNum = rowNum + 1;
     end
     At = A.';
     Bt = B.';
-    [U, S, V] = svd((At * A) + (Bt * B) - (Bt * A) - (At * B));
+    [~, ~, V] = svd((At * A) + (Bt * B) - (Bt * A) - (At * B));
     m = V(:,end);
     count = 1;
     for y = 1:3
@@ -72,12 +73,19 @@ function homogenousMatrix = FindTransformationMatrixWithPoints(points, points2)
     
     %For sanity checking, transform a random point and see if it is correct!
     testPointNum = randi(4);
-    testPoint = points(testPointNum,:);
-    testPointPrime = points2(testPointNum, :);
+    testPoint = basePoints(testPointNum,:);
+    testPointPrime = primePoints(testPointNum, :);
     transformTestPoint = TransformPoint(testPoint, homogenousMatrix);
     isTransformationCorrect = isequal(testPointPrime, transformTestPoint);
     if (~isTransformationCorrect) 
         disp("Something went wrong finding the transformation matrix!");
+    end
+end
+
+function transformedPoints = TransformMultiplePoints(points, transformationMatrix)
+    transformedPoints = zeros(size(points, 1), size(points, 2));
+    for pointNum = 1:size(points,1)
+        transformedPoints(pointNum, :) = TransformPoint(points(pointNum,:), transformationMatrix);
     end
 end
 
@@ -90,4 +98,88 @@ function transformedPoint = TransformPoint(point, transformationMatrix)
         ];
     homogenousTransformedPoint = transformationMatrix * homogenousPoint;
     transformedPoint = [round(homogenousTransformedPoint(1,:)/homogenousTransformedPoint(3,:)), round(homogenousTransformedPoint(2,:)/homogenousTransformedPoint(3,:))];
+end
+
+function stitchedImage = StitchImages(baseImage, transformedImage, transformationMatrix)
+    %Points are still in (x,y) format
+    transformedImageCornerPoints = [
+        1 1;                                                %Top Left
+        1 size(transformedImage, 1);                        %Bottom Left
+        size(transformedImage, 2) 1;                        %Top Right
+        size(transformedImage, 2) size(transformedImage, 1) %Bottom Right
+    ];
+    transformedImageTransformedCornerPoints = TransformMultiplePoints(transformedImageCornerPoints, inv(transformationMatrix));
+    minTransformedY = min(transformedImageTransformedCornerPoints(1,2), transformedImageTransformedCornerPoints(3,2));
+    maxTransformedY = max(transformedImageTransformedCornerPoints(2,2), transformedImageTransformedCornerPoints(4,2));
+    minTransformedX = min(transformedImageTransformedCornerPoints(1,1), transformedImageTransformedCornerPoints(2,1));
+    maxTransformedX = max(transformedImageTransformedCornerPoints(3,1), transformedImageTransformedCornerPoints(4,1));
+    transformedImageTransformedHeight = maxTransformedY - minTransformedY;
+    transformedImageTransformedWidth = maxTransformedX - minTransformedX;
+    minBaseImageY = 0;
+    maxBaseImageY = size(baseImage, 1);
+    minBaseImageX = 0;
+    maxBaseImageX = size(baseImage, 2);
+    baseImageHeight = size(baseImage, 1);
+    baseImageWidth = size(baseImage, 2);
+    
+    %Setup canvas for the stitched image
+    minStitchedImageY = min(minBaseImageY, minTransformedY);
+    maxStitchedImageY = max(maxBaseImageY, maxTransformedY);
+    minStitchedImageX = min(minBaseImageX, minTransformedX);
+    maxStitchedImageX = max(maxBaseImageX, maxTransformedX);
+    stitchedImageHeight = maxStitchedImageY - minStitchedImageY;
+    stitchedImageWidth = maxStitchedImageX - minStitchedImageX;
+    stitchedImage = uint8(zeros(stitchedImageHeight, stitchedImageWidth, 3));
+
+    for y = 1:stitchedImageHeight
+        for x = 1:stitchedImageWidth
+            if (minTransformedX < 1)
+                baseImageX = x + minTransformedX;
+            else
+                baseImageX = x;
+            end
+
+            if (minTransformedY < 1)
+                baseImageY = y + minTransformedY;
+            else
+                baseImageY = y;
+            end
+
+            baseImagePoint = [baseImageX, baseImageY];
+            transformedImagePoint = TransformPoint(baseImagePoint, transformationMatrix);
+            transformedImageY = transformedImagePoint(2);
+            transformedImageX = transformedImagePoint(1);
+            
+            pointExistsInBaseImage = PointInImage(baseImagePoint, baseImage);
+            pointExistsInTransformedImage = PointInImage(transformedImagePoint, transformedImage);
+
+            if (~pointExistsInBaseImage)
+                baseImagePointColor = [0, 0, 0];
+            else
+                baseImagePointColor = [baseImage(baseImageY, baseImageX, 1) baseImage(baseImageY, baseImageX, 2) baseImage(baseImageY, baseImageX, 3)];
+            end
+
+            if (~pointExistsInTransformedImage)
+                transformedImagePointColor = [0, 0, 0];
+            else
+                transformedImagePointColor = [transformedImage(transformedImageY, transformedImageX, 1) transformedImage(transformedImageY, transformedImageX, 2) transformedImage(transformedImageY, transformedImageX, 3)];
+            end
+            
+            if (pointExistsInBaseImage && pointExistsInTransformedImage)
+                stitchedImage(y,x,:) = uint8(BlendPointColors(baseImagePointColor, transformedImagePointColor, (1 - baseImageX/baseImageWidth)));
+            elseif (pointExistsInTransformedImage)
+                stitchedImage(y,x,:) = uint8(transformedImagePointColor);
+            else
+                stitchedImage(y,x,:) = uint8(baseImagePointColor);
+            end
+        end
+    end
+end 
+
+function existsInImage = PointInImage (point, image)
+    existsInImage = (point(1) > 0) && (point(1) <= size(image,2)) && (point(2) > 0) && (point(2) <= size(image,1));
+end
+
+function blendedPointColors = BlendPointColors (point1Colors, point2Colors, alpha)
+    blendedPointColors = [point1Colors(1)*alpha+point2Colors(1)*(1-alpha) point1Colors(2)*alpha+point2Colors(2)*(1-alpha) point1Colors(3)*alpha+point2Colors(3)*(1-alpha)];
 end
